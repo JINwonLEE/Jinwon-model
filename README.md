8af05a4d9802bb30534a5a6a167cb0156391911f
Jinwon Lee <dlwlsdnjsehs1993@gmail.com>
2019-02-05 17:55:00 +0900

    data processing

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Jinwon-model
 Models implemented by Jinwon Lee
+
+DataSet :
+    Data set is located in another directory
+
+MNIST : 28 X 28, 60000, 10000 => 44.8MB, 7.5MB
+Cifar10 : 32 X 32, 50000, 10000 => 
+Cifar100 : 32 X 32, 50000, 10000 =>
---
+++ data_processing.py
@@ -0,0 +1,46 @@
+# coding=utf-8
+import numpy as np
+import os
+import tarfile
+import zipfile
+import urllib
+
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+def try_download_and_extract(name="mnist"):
+    directory = "/Users/jwl1993/dataset/"
+    data_url = None
+    if name == "mnist":
+        data_name = "mnist/"
+    elif name == "cifar10":
+        data_name = "cifar10/"
+        data_url = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
+    elif name == "cifar100":
+        data_name = "cifar100/"
+        data_url = "https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz"
+
+    data_directory = directory
+
+    if not os.path.exists(data_directory + data_name) and name != "mnist" :
+        print("There is no data set of %s, so we try to download" %name)
+        os.makedirs(data_directory)
+
+        compressed_file = data_url.split("/")[-1]
+        location = os.path.join(data_directory, compressed_file)
+        download_file, _ = urllib.urlretrieve(url=data_url, filename=location)
+        print("Download done")
+
+        if download_file.endswith(".zip") :
+            zipfile.ZipFile(file=download_file, mode="r").extractall(directory)
+        elif download_file.endswith(".tar.gz") :
+            tarfile.open(name=download_file, mode="r:gz").extractall(directory)
+
+        print("Extract done")
+
+        os.remove(location)
+
+#def get_dataset(name="mnist", mode="train") :
+
+
