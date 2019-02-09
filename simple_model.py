fcbdb79e2ae91a7ce414cfbb1b8c5939dbeb71b7
Jinwon Lee <dlwlsdnjsehs1993@gmail.com>
2019-02-09 16:39:00 +0900

    Model Construction

--- simple_model.py
+++ simple_model.py
@@ -11,7 +11,7 @@ import data_processing
 
 
 learning_rate = 0.01
-batch_size = 128  
+batch_size = 128
 number_of_iteration = 1000
 
 hidden1 = 256
@@ -25,14 +25,12 @@ def weight_variable(shape, name=None) :
     w_name = 'Weight' + name
     weight = tf.get_variable(w_name, dtype=tf.float32, 
                                 shape=shape, initializer=init)
-    #weight = tf.Variable(dtype=tf.float32, 
     return weight
 
 def bias_variable(shape, name=None) :
     init = tf.constant(0, shape=shape, dtype=tf.float32)
 
     b_name = 'Bias' + name
-    #bias = tf.Variable(dtype=tf.float32, 
     bias = tf.get_variable(b_name, dtype=tf.float32, 
                                  initializer=init)
     return bias 
@@ -79,25 +77,46 @@ def model_fn(features, labels, mode) :
         logits=logit, labels=tf.cast(labels, dtype=tf.int32)))
 
     optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+
+    global_step = tf.train.get_global_step()
     train_op = optimizer.minimize(loss_op,
-                                  global_step=tf.train.get_global_step())
+                                  global_step=global_step)
 
     accuracy = tf.metrics.accuracy(labels=labels, predictions=predict_class)
+
+# Create hook list which is for logging some losses, accuracy in certain step 
+    train_hook_list = []
+    train_tensor_log = {'accuracy_1' : accuracy[1], 'loss' : loss_op, 'global_step' : global_step}
+    train_hook_list.append(tf.train.LoggingTensorHook(tensors=train_tensor_log, every_n_iter=100))
 
     estim_specs = tf.estimator.EstimatorSpec(
         mode=mode,
         predictions=predict_class,
         loss=loss_op,
         train_op=train_op,
+        training_hooks=train_hook_list,
         eval_metric_ops={'accuracy': accuracy})
 
     return estim_specs
 
+# Visualize the process of training steps
+tf.logging.set_verbosity(tf.logging.INFO)
+
+# Try getting the dataset if there is no data set in local 
 data_processing.try_download_and_extract()
+
+# This is the case of MNIST
+# In case of other data set (Cifar10, Cifar100) you have to check
 mnist = data_processing.get_dataset()
 
-model = tf.estimator.Estimator(model_fn, model_dir="/Users/jwl1993/model_dir")
+config = None
+# Logging Device placement 
+#config=tf.estimator.RunConfig(session_config=tf.ConfigProto(log_device_placement=True))
 
+# Create Estimator with model directory [XXX] Model Directory check
+model = tf.estimator.Estimator(model_fn, model_dir="/Users/jwl1993/model_dir", config=config)
+
+# Training input, if num_epochs == None, training will run forever until the max_step is set
 input_fn = tf.estimator.inputs.numpy_input_fn(
     x=mnist.train.images, y=mnist.train.labels,
     batch_size=batch_size, num_epochs=None, shuffle=True)
@@ -105,6 +124,7 @@ input_fn = tf.estimator.inputs.numpy_input_fn(
 _ = model.train(input_fn, steps=number_of_iteration)
 
 
+# Test input
 input_fn = tf.estimator.inputs.numpy_input_fn(
     x=mnist.test.images, y=mnist.test.labels,
     batch_size=batch_size, shuffle=False)
